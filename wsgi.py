75da51215be5c03be776d64ab9943aa24b9e611e
Miguel Sánchez de León Peque <peque@neosit.es>
2016-06-06 00:01:04 +0200

    Enable OpenShift deployment

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+from soundphy import app as application
+
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    HOST, PORT = 'localhost', 5000
+    print('Serving at http://%s:%s' % (HOST, PORT))
+    make_server(HOST, PORT, application).serve_forever()
