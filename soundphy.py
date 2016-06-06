3dabe36581a471fea69700a055698993841ab6cf
Miguel Sánchez de León Peque <peque@neosit.es>
2016-06-06 20:58:24 +0200

    Playing around with RESTful API and error responses

--- soundphy.py
+++ soundphy.py
@@ -1,12 +1,31 @@
+"""
+Soundphy RESTful API.
+"""
 from flask import Flask
+from flask import abort
+from flask import request
+from flask import jsonify
 
 
 app = Flask('Soundphy')
 
 
+@app.errorhandler(400)
+def handle_400(e):
+    return jsonify(error=str(e))
+
+
 @app.route('/')
 def root():
     return 'Hello World!'
+
+
+@app.route('/api/v0.1/reverse')
+def reverse():
+    query = request.args.get('query')
+    if query is None:
+        abort(400)
+    return jsonify(reverse=query[::-1])
 
 
 if __name__ == '__main__':
