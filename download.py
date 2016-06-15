4b3b323b22b28bcaeb48efed2c034c4474a16ee1
Miguel Sánchez de León Peque <peque@neosit.es>
2016-06-16 00:21:24 +0200

    Initial Dota 2 sound support

--- download.py
+++ download.py
@@ -1,6 +1,7 @@
 import os
 import re
 import requests
+from bs4 import BeautifulSoup
 from whoosh.fields import ID
 from whoosh.fields import TEXT
 from whoosh.fields import NGRAMWORDS
@@ -21,6 +22,37 @@ def download_instantsfun_es():
     return zip(urls, descriptions)
 
 
+def download_dota2_gamepedia_com():
+    WEBPAGE = 'http://dota2.gamepedia.com'
+
+    # Heroes
+    response = requests.get(WEBPAGE + '/Hero_Grid')
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    heroes = soup.find_all('div', attrs={'style': 'position: relative;'})
+
+    pages = [hero.find('a').attrs['href'] for hero in heroes]
+    names = [hero.find('a').attrs['title'] for hero in heroes]
+    faces = [hero.find('img').attrs['src'] for hero in heroes]
+
+    responses = []
+    for page in pages:
+        response = requests.get(WEBPAGE + page + '/Responses')
+        try:
+            response.raise_for_status()
+        except Exception:
+            print(page)
+            continue
+        soup = BeautifulSoup(response.text, 'html.parser')
+        links = soup.find_all('a', attrs={'title': 'Play'})
+        sounds = [link.attrs['href'] for link in links]
+        descriptions = [link.parent.text.strip(' Play ') for link in links]
+        responses.append(zip(sounds, descriptions))
+
+    return zip(names, faces, responses)
+
+
 def create_schema(index_directory):
     schema = Schema(url=ID(stored=True),
                     title=TEXT(stored=True),
@@ -33,6 +65,11 @@ def create_schema(index_directory):
         writer.add_document(url=url,
                             title=description,
                             description=description)
+    for name, face, responses in download_dota2_gamepedia_com():
+        for sound, description in responses:
+            writer.add_document(url=sound,
+                                title='Dota2 %s '%name+description,
+                                description='Dota2 %s '%name+description)
     writer.commit()
 
 
